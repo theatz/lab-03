b3714f01c504f1aec1d577873ef96950b2d6f615
theatz <Ljvbybrfyf36>
2020-12-15 00:31:23 +0300

    com1

---
+++ include/SharedPtr.hpp
@@ -0,0 +1,169 @@
+//
+// Created by mrbgn on 14.12.2020.
+//
+
+#ifndef TEMPLATE_SHAREDPTR_HPP
+#define TEMPLATE_SHAREDPTR_HPP
+#include <atomic>
+#include <iostream>
+#include <utility>
+
+template <typename T>
+class SharedPtr {
+ public:
+
+  SharedPtr() {
+    _ptr = nullptr;
+    _control = nullptr;
+  }
+
+  explicit SharedPtr(T* ptr)
+  {
+    _ptr = ptr;
+    if (_ptr == nullptr)
+    {
+      _control = nullptr;
+    } else {
+      _control = new std::atomic_uint;
+      *_control = 1;
+    }
+  }
+
+  SharedPtr(const SharedPtr& copy)
+  {
+    _ptr = copy._ptr;
+    _control = copy._control;
+    if (_ptr) {
+      *_control = *_control + 1;
+    } else {
+      _control = nullptr;
+    }
+  }
+
+  SharedPtr(SharedPtr&& move)
+  {
+    _ptr = move._ptr;
+    _control = move._control;
+    move._ptr = nullptr;
+    move._control = nullptr;
+  }
+
+  ~SharedPtr()
+  {
+    if (_ptr){
+      if (*_control == 1)
+      {
+        delete _ptr;
+        delete _control;
+      } else {
+        *_control = *_control - 1;
+      }
+    }
+    _ptr = nullptr;
+    _control = nullptr;
+  }
+
+  auto operator=(const SharedPtr& eq_copy) -> SharedPtr&
+  {
+    if (this != &eq_copy) {
+      //
+      _ptr = eq_copy._ptr;
+      _control = eq_copy._control;
+      if (_ptr) {
+        *_control = *_control + 1;
+      } else {
+        _control = nullptr;
+      }
+    }
+    return *this;
+  }
+
+  auto operator=(const SharedPtr&& eq_move) -> SharedPtr&
+  {
+    if (this != &eq_move){
+      _ptr = eq_move._ptr;
+      _control = eq_move._control;
+    }
+    return *this;
+  }
+
+  // проверяет, указывает ли указатель на объект
+  operator bool() const
+  {
+    if (_ptr) return true;
+    else
+      return false;
+  }
+
+  auto operator*() const -> T&
+  {
+    if (_ptr) return *_ptr;
+    else
+      return nullptr;
+  }
+  auto operator->() const -> T*
+  {
+    return _ptr;
+  }
+
+  auto get() -> T*
+  {
+    return _ptr;
+  }
+
+  void reset()
+  {
+    if (*_control == 1)
+    {
+      delete _ptr;
+      delete _control;
+    } else {
+      *_control = *_control - 1;
+    }
+    _ptr = nullptr;
+    _control = nullptr;
+  }
+
+  void reset(T* ptr)
+  {
+    if (*_control == 1)
+    {
+      delete _ptr;
+      delete _control;
+    } else {
+      *_control = *_control - 1;}
+    _ptr = ptr;
+    if (ptr == nullptr)
+    {
+      _control = nullptr;
+    } else {
+      _control = new std::atomic_uint;
+      *_control = 1;
+    }
+  }
+
+  void swap(SharedPtr& r)
+  {
+    T* tmp_ptr(std::move(r._ptr));
+    r._ptr = std::move(_ptr);
+    _ptr = std::move(tmp_ptr);
+    std::atomic_uint* tmp_control = r._control;
+    r._control = _control;
+    _control = tmp_control;
+  }
+
+  // возвращает количество объектов SharedPtr, которые ссылаются на тот же управляемый объект
+  auto use_count() const -> size_t
+  {
+    if (_control) {
+      return *_control;
+    }
+    else return 0;
+  }
+
+ private:
+  T* _ptr;
+  std::atomic_uint* _control;
+};
+
+#endif  // TEMPLATE_SHAREDPTR_HPP
--- tests/test.cpp
+++ tests/test.cpp
@@ -1,7 +1,109 @@
 // Copyright 2020 Your Name <your_email>
 
 #include <gtest/gtest.h>
+#include "SharedPtr.hpp"
 
 TEST(Example, EmptyTest) {
-    EXPECT_TRUE(true);
+  EXPECT_TRUE(true);
+}
+
+TEST(test_constructor, point_nullptr) {
+  SharedPtr <int> ptr;
+  int* tmp = ptr.get();
+  EXPECT_EQ(tmp, nullptr);
+}
+
+TEST(test_constructor, point_int_nullptr) {
+  int *points = new int (24);
+  SharedPtr <int> ptr(points);
+  int* tmp = ptr.get();
+  EXPECT_EQ(tmp, points);
+}
+
+TEST(test_method, test_get) {
+  int *points = nullptr;
+  SharedPtr <int> ptr(points);
+  int* tmp = ptr.get();
+  EXPECT_EQ(tmp, nullptr);
+}
+
+TEST(test_method, test_get_int) {
+  int *points = new int (227);
+  SharedPtr <int> ptr(points);
+  SharedPtr <int> ptr2 = ptr;
+  EXPECT_EQ(ptr.get(), points);
+}
+
+TEST(test_operator, bool_test) {
+  int *points = new int (227);
+  SharedPtr <int> ptr(points);
+  SharedPtr <int> ptr2 = ptr;
+  bool is_pointer = ptr2;
+  EXPECT_EQ(is_pointer, true);
+  }
+
+class Test_class
+{
+ public:
+  int x;
+};
+
+TEST(test_operator, arrow_test) {
+  auto test_class_ptr = new Test_class;
+  test_class_ptr ->x = 227;
+  SharedPtr <Test_class> ptr(test_class_ptr);
+  EXPECT_EQ(ptr->x, test_class_ptr->x);
+}
+
+TEST(test_operator, equality_lvalue_test) {
+  int *points = new int (227);
+  SharedPtr <int> ptr(points);
+  SharedPtr <int> ptr2;
+  SharedPtr <int> ptr3 = ptr2 = ptr; //
+  EXPECT_EQ(ptr3.get(), ptr.get());
+}
+
+TEST(test_constructor, rvalue_test) {
+  int *points = new int (227);
+  SharedPtr <int> ptr(points);
+  SharedPtr <int> ptr2 = ptr;
+  SharedPtr <int> ptr3(std::move(ptr));
+  EXPECT_EQ(ptr3.use_count(), 2);
+}
+
+TEST(test_method, reset_test) {
+  int  *points = new int (227);
+  SharedPtr <int> ptr(points);
+  ptr.reset();
+  EXPECT_EQ(ptr.get(), nullptr);
+}
+
+TEST(test_method, reset_test_point) {
+  int *points = new int (227);
+  int *points1 = new int (200);
+  SharedPtr <int> ptr(points);
+  ptr.reset(points1);
+  EXPECT_EQ(ptr.get(), points1);
+}
+
+TEST(test_method, swap_test) {
+  int *points = new int (227);
+  int *points1 = new int (200);
+  SharedPtr <int> ptr(points);
+  SharedPtr <int> ptr1(points1);
+  ptr.swap(ptr1);
+  EXPECT_EQ(ptr.get(), points1);
+  EXPECT_EQ(ptr1.get(), points);
+}
+
+TEST(test_is_move, assign_construct_test) {
+  EXPECT_EQ(std::is_move_assignable<SharedPtr<int>>::value, true);
+  EXPECT_EQ(std::is_move_constructible<SharedPtr<int>>::value, true);
+  EXPECT_EQ(std::is_move_assignable<SharedPtr<double>>::value, true);
+  EXPECT_EQ(std::is_move_constructible<SharedPtr<double>>::value,
+  true);
+  EXPECT_EQ(std::is_move_assignable<SharedPtr<Test_class>>::value,
+  true);
+  EXPECT_EQ(std::is_move_constructible<SharedPtr<Test_class>>::value,
+  true);
 }
